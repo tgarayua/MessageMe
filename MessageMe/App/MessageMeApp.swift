9c0da2bc7520d9c2e3af93d0a64129fce386053e
Thomas Garayua <tgarayua@gmail.com>
2024-01-29 22:02:47 -0500

    first commit

---
+++ MessageMe/App/MessageMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageMeApp.swift
+//  MessageMe
+//
+//  Created by Thomas Garayua on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MessageMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessageMe/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,34 @@
+//
+//  LoginView.swift
+//  MessageMe
+//
+//  Created by Thomas Garayua on 1/29/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // logo image
+                
+                // text fields
+                
+                // forgot password
+                
+                // login button
+                
+                // facebook login
+                
+                // sign up link
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
