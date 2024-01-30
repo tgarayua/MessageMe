e5fdd7cf04ca2b99ddafe6fff2f5a054f6668c34
Thomas Garayua <tgarayua@gmail.com>
2024-01-30 12:13:14 -0500

    Completed Profile Photo Selection Functionality

---
+++ MessageMe/Core/NewMessage/NewMessageView.swift
@@ -0,0 +1,67 @@
+//
+//  NewMessageView.swift
+//  MessageMe
+//
+//  Created by Thomas Garayua on 1/30/24.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    @State private var searchText = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                TextField("To: ", text: $searchText)
+                    .frame(height: 44)
+                    .padding(.leading)
+                    .background(Color(.systemGroupedBackground))
+                
+                Text("CONTACTS")
+                    .foregroundColor(.gray)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                ForEach(0 ... 30, id: \.self) { user in
+                    VStack {
+                        HStack {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .foregroundColor(Color(.systemGray4))
+                            
+                            Text("Chadwick Bozeman")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            
+                            Spacer()
+                        }
+                        .padding(.leading)
+                        
+                        Divider()
+                            .padding(.leading, 40)
+                    }
+                }
+            }
+            .navigationTitle("New Message")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+struct NewMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewMessageView()
+    }
+}
---
+++ MessageMe/Core/NewMessage/ProfileView.swift
@@ -0,0 +1,73 @@
+//
+//  ProfileView.swift
+//  MessageMe
+//
+//  Created by Thomas Garayua on 1/30/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewModel()
+    
+    var body: some View {
+        VStack {
+            // header
+            VStack {
+                PhotosPicker(selection: $viewModel.selectedItem) {
+                    if let profileImage = viewModel.profileImage {
+                        profileImage
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                    } else {
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(Color(.systemGray4))
+                    }
+                }
+                
+                Text("Bruce Wayne")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+            
+            // list
+            
+            List {
+                Section {
+                    ForEach(SettingsOptionsViewModel.allCases) { option in
+                        HStack {
+                            Image(systemName: option.imageName)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(option.imageBackgroundColor)
+                            
+                            Text(option.title)
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                Section {
+                    Button("Log Out") {
+                        
+                    }
+                    
+                    Button("Delete Account") {
+                        
+                    }
+                }
+                .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
