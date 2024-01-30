886777c81e6850baecc66d7c8530e3f7901b8d8c
Thomas Garayua <tgarayua@gmail.com>
2024-01-30 18:34:26 -0500

    Upload/Download User Data

--- MessageMe/Core/Authentication/Service/AuthService.swift
+++ MessageMe/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthService {
     
@@ -16,9 +17,11 @@ class AuthService {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        Task { try await UserService.shared.fetchCurrentUser() }
         print("DEBUG: User session id is \(userSession?.uid)")
     }
     
+    @MainActor
     func login(withEmail email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -28,9 +31,12 @@ class AuthService {
         }
     }
     
+    @MainActor
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            try await self.uploadUserData(email: email, fullname: fullname, id: result.user.uid)
             print("DEBUG: Create user \(result.user.uid)")
         } catch {
             print("DEBUG: Failed to create user with error: \(error.localizedDescription)")
@@ -44,5 +50,11 @@ class AuthService {
         } catch {
             print("DEBUG: Failed to sign out with error \(error.localizedDescription)")
         }
+    }
+    
+    private func uploadUserData(email: String, fullname: String, id: String) async throws {
+        let user = User(fullname: fullname, email: email, profileImageUrl: nil)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try await Firestore.firestore().collection("users").document(id).setData(encodedUser)
     }
 }
